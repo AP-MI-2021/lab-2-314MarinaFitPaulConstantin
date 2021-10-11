f6b207b47bdbc57c058631cdb77255c1ff6558dc
314MarinaFitPaulConstantin <91897079+314MarinaFitPaulConstantin@users.noreply.github.com>
2021-10-11 09:43:22 +0300

    Add files via upload
    Calculează combinări de n luate câte k (n și k date).
    
    Funcția principală: get_n_choose_k(n: int, k: int) -> int
    Funcția de test: test_get_n_choose_k()

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-def is_palindrome(n):
+def get_n_choose_k(n,k):
+    rezultat = 1
+    for i in range(1,k+1):
+        rezultat = rezultat * (n-(k-i))//i
+    return rezultat
 
 
- #verific pas cu pas fiecare element al numarului introdus, n, pentru a putea afirma daca este sau nu palindrom
-    for i in range(0, int(len(n) / 2)):
-        if n[i] != n[len(n) - i - 1]:
-  # returnez valaorea de adevar a functiei is_palindrom
-            return False
-    return True
+
+def test_get_n_choose_k(n,k):
+  assert get_n_choose_k(5, 3) == 10
+  assert get_n_choose_k(10, 5) == 252
+  assert get_n_choose_k(6, 4) == 15
 
 
-def test_is_palindrom(n):
-    assert is_palindrome(55) is True
-    assert is_palindrome(101) is True
-    assert is_palindrome(12) is False
-
-n=input("Introdu numar: ")
-print(is_palindrome(n))
+n = int(input("Dati n: "))
+k = int(input("Dati k: "))
+print(get_n_choose_k(n,k))
