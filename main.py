8839449228ef4a59e957d21df7beee2315666e50
314MarinaFitPaulConstantin <91897079+314MarinaFitPaulConstantin@users.noreply.github.com>
2021-10-11 10:01:44 +0300

    Update main.py
    problema 5,10

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+#problema 5
+#verific daca un numar este palindrom
+def is_palindrome(n):
+
+ #verific pas cu pas fiecare element al numarului introdus, n, pentru a putea afirma daca este sau nu palindrom
+    for i in range(0, int(len(n) / 2)):
+        if n[i] != n[len(n) - i - 1]:
+  # returnez valaorea de adevar a functiei is_palindrom
+            return False
+    return True
+#fac niste teste sa vad daca programmu ruleaza bine
+def test_is_palindrom(n):
+    assert is_palindrome(55) is True
+    assert is_palindrome(101) is True
+    assert is_palindrome(12) is False
+
+n=input("Introdu numar: ")
+print(is_palindrome(n))
+
+
+#problema 10
+#calculez combinari de n luate cate k
 def get_n_choose_k(n,k):
     rezultat = 1
     for i in range(1,k+1):
         rezultat = rezultat * (n-(k-i))//i
     return rezultat
-
-
-
+#fac niste teste sa vad daca programmu ruleaza bine
 def test_get_n_choose_k(n,k):
   assert get_n_choose_k(5, 3) == 10
   assert get_n_choose_k(10, 5) == 252
